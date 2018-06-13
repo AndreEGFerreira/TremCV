0c4426b2b6dc7b7b6dd64d857ba7b13d61c477b6
AndreEGFerreira <32279586+AndreEGFerreira@users.noreply.github.com>
2018-06-13 09:58:49 -0300

    Add files via upload
    Iniciei de verdade o projeto.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,22 @@
 
 using namespace std;
 
+int buscaTransicaoAtiva(MapaTrem mapa){
+
+    for(int j=0; j<16; j++){
+        for(int i=0; i<17; i++){
+            if(preTrans[i][j] != mAtual[i])
+                break;
+            if(i == 16 && preTrans[i][j] == mAtual[i]){
+                return j;
+            }
+        }
+    }
+}
+
 int main(int argc, char **argv) {
-    MapaTrem trens; 
-    
+    MapaTrem trens;
+
     // posições para os trens
     vector<Point> pt1;
     pt1.push_back(Point(25, 158));
@@ -16,7 +29,7 @@ int main(int argc, char **argv) {
     pt1.push_back(Point(304, 242));
     pt1.push_back(Point(435, 242));
     pt1.push_back(Point(475, 242));
-    
+
     vector<Point> pt2;
     pt2.push_back(Point(25, 326));
     pt2.push_back(Point(67, 326));
@@ -24,35 +37,85 @@ int main(int argc, char **argv) {
     pt2.push_back(Point(304, 242));
     pt2.push_back(Point(435, 242));
     pt2.push_back(Point(475, 242));
-    
+
     //trens.Trem1Pos(200,200);
     //trens.Trem2Pos(100,200);
     //trens.Trem1Txt("Mensagem trem 1.");
     //trens.Trem2Txt("Mensagem trem 2.");
-    
+
+    redePetri[17] = {0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0};
+    mAtual[17];
+    posTrans[17][16] = {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+                        0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,};
+
+    preTrans[17][16] = {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,
+                        0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,
+                        0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,
+                        0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                        0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+                        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,
+                        0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,};
+
+
+    for(int i=0; i<17; i++){
+        mAtual[i] = redePetri[i];
+    }
+
+    while (1) {
+        buscaTransicaoAtiva();
+    }
+
+    // A CADA ITERAÇÃO VER DE COLUNA EM COLUNA SE EH POSSIVEL LANCAR A TRANSICAO
+    // PEGA A COLUNA preTrans E QUANDO ACHAR 1, PROCURAR SE NO VETOR TEM 1 TBM, ENTAO DA PARA FAZER
+
     trens.Gate(1);
     for(Point p : pt1)
     {
-        trens.Trem1Pos(p.x,p.y);
-        Thread::SleepMS(3000);
-        
+        trens.Trem1Pos(p.x,p.y);  //como pegar posição +-
+        Thread::SleepMS(3000);    //tempo SLEEP NORMAL
+
         int key = trens.GetLastKey();
         cout << "LastKey = " << key << endl;
         if(key == 27)
             return 1;
     }
-    
+
     trens.Gate(0);
     for(Point p : pt2)
     {
         trens.Trem2Pos(p.x,p.y);
         Thread::SleepMS(3000);
-        
+
         int key = trens.GetLastKey();
         cout << "LastKey = " << key << endl;
         if(key == 27)
             return 1;
     }
-    
+
     return 0;
 }
